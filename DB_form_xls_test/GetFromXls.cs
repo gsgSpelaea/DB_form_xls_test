299d65fd8fa4001debdab85fe7cdb31534cfcf26
kir222k <u.k.send@gmail.com>
2021-02-05 00:36:45 +0300

    Hi! See  reading from *.xlsx file to DataSet object. Concole App.

---
+++ DB_form_xls_test/GetFromXls.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExcelDataReader;
+using System.IO;
+using System.Data;
+
+
+namespace DB_form_xls_test
+{
+    public class GetFromXls
+    {
+
+        public string FilePath { get; set; }
+
+
+        public GetFromXls(string filePath) { FilePath = filePath; }
+    //public filepath {get;set;};
+
+
+
+    public DataSet GetDataFromXls()//(string filePath)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            DataSet result = null;
+
+            using (var stream = File.Open(this.FilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+                using (var reader = ExcelReaderFactory.CreateReader(stream,
+                    new ExcelReaderConfiguration()
+                    {
+                        FallbackEncoding = Encoding.GetEncoding(1252)
+
+                    }))
+                {
+                    // Choose one of either 1 or 2:
+
+                    // 1. Use the reader methods
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            // reader.GetDouble(0);
+                        }
+                    } while (reader.NextResult());
+
+                    // 2. Use the AsDataSet extension method
+                     result = reader.AsDataSet();
+
+                    // The result of each spreadsheet is in result.Tables
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
--- DB_form_xls_test/Program.cs
+++ DB_form_xls_test/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Data;
+using System.Linq;
+using System;
 
 namespace DB_form_xls_test
 {
@@ -6,7 +8,33 @@ namespace DB_form_xls_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            GetFromXls GetXls = new GetFromXls("u:\\dev\\ANYC\\DB_form_xls_test\\DataToCS.xlsx");
+            DataSet Ds = GetXls.GetDataFromXls();
+
+            Console.WriteLine($"\nDataTable.Count: {Ds.Tables.Count}");
+
+
+            foreach (DataTable dt  in Ds.Tables)
+            {
+                Console.WriteLine($"\ndt.TableName: {dt.TableName}");
+                Console.WriteLine($"\ndt.Rows.Count: {dt.Rows.Count}");
+                Console.WriteLine($"\ndt.Columns.Count: {dt.Columns.Count}");
+
+                // foreach ( DataRow dr in table.Rows )
+                for (int i = 0; i < dt.Rows.Count; i++) 
+                {
+
+                    string strRec = "";
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        strRec +=  dt.Rows[i][j].ToString() + " ";
+                    }
+                    Console.WriteLine(strRec);
+                }
+            }
+            Console.ReadKey();
+
         }
     }
 }
