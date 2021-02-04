ae7c9981611cee923855ece86a755cf286e77920
kir222k <u.k.send@gmail.com>
2021-02-04 22:53:40 +0300

    Add project files.

---
+++ DB_form_xls_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DB_form_xls_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
